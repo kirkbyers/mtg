13df44cb610b489e7066dfa8fb7e37451e75fa0e
kirkbyers <kirk@cratejoy.com>
2024-07-22 22:53:20 -0500

    Working api/cards.

--- src/db.rs
+++ src/db.rs
@@ -1,4 +1,5 @@
 use rusqlite::Connection;
+use serde::{Serialize, Deserialize};
 
 pub fn init_conn() -> Result<Connection, Box<dyn std::error::Error>> {
     let conn = Connection::open("./data/scryfall_cards.db")?;
@@ -116,4 +117,79 @@ pub fn get_random_image_uris(
     } else {
         Err("No image URIs found".into())
     }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Card {
+    pub id: String,
+    pub oracle_id: String,
+    pub name: String,
+    pub lang: Option<String>,
+    pub released_at: Option<String>,
+    pub mana_cost: Option<String>,
+    pub cmc: Option<f64>,
+    pub type_line: Option<String>,
+    pub oracle_text: Option<String>,
+    pub power: Option<String>,
+    pub toughness: Option<String>,
+    pub rarity: Option<String>,
+    pub flavor_text: Option<String>,
+    pub artist: Option<String>,
+    pub set_code: Option<String>,
+    pub collector_number: Option<String>,
+    pub digital: Option<String>,
+}
+
+pub fn search_cards(
+    conn: &Connection,
+    query: &str,
+    page: u32,
+    page_size: u32,
+) -> Result<Vec<Card>, Box<dyn std::error::Error>> {
+    let offset = (page - 1) * page_size;
+    let limit = page_size;
+
+    let mut stmt_str = String::from("SELECT id, oracle_id, name, lang, released_at, mana_cost, cmc, type_line, oracle_text, power, toughness, rarity, flavor_text, artist, set_code, collector_number, digital FROM cards ");
+
+    if !query.is_empty() {
+        stmt_str += "WHERE name LIKE ? ";
+    }
+
+    stmt_str += "ORDER BY name LIMIT ? OFFSET ?;";
+
+    let mut stmt = conn.prepare(
+        &stmt_str
+    )?;
+    let mut rows = if query.is_empty() {
+        stmt.query(&[&limit.to_string(), &offset.to_string()])?
+    } else {
+        stmt.query(&[query, &limit.to_string(), &offset.to_string()])?
+    };
+
+    let mut results = Vec::new();
+    while let Some(row) = rows.next()? {
+        let card = Card {
+            id: row.get(0)?,
+            oracle_id: row.get(1)?,
+            name: row.get(2)?,
+            lang: row.get(3)?,
+            released_at: row.get(4)?,
+            mana_cost: row.get(5)?,
+            cmc: row.get(6)?,
+            type_line: row.get(7)?,
+            oracle_text: row.get(8)?,
+            power: row.get(9)?,
+            toughness: row.get(10)?,
+            rarity: row.get(11)?,
+            flavor_text: row.get(12)?,
+            artist: row.get(13)?,
+            set_code: row.get(14)?,
+            collector_number: row.get(15)?,
+            digital: row.get(16)?,
+        };
+
+        results.push(card);
+    }
+
+    Ok(results)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,67 @@
-use axum::Router;
+use mtg::db::{init_conn, search_cards, Card};
+use rusqlite::Connection;
 use tower_http::services::ServeDir;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use axum::{extract::{Query, State}, routing::get, Json, Router};
+use serde::Deserialize;
+
+// Wrapper for SQLite connection
+struct DbConnection(Mutex<Connection>);
 
 #[tokio::main]
 async fn main() {
+    let conn = init_conn().expect("Failed to init db");
+    let db = Arc::new(DbConnection(Mutex::new(conn)));
     // Create a new router
-    let app = Router::new().nest_service("/", ServeDir::new("www"));
+    let app = Router::new()
+        .route("/api/cards", get(get_cards))
+        .nest_service("/", ServeDir::new("www"))
+        .with_state(db);
 
     // Start the server
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+#[derive(Deserialize)]
+struct CardQueryParams {
+    #[serde(default = "default_page")]
+    page: u32,
+    #[serde(default = "default_limit")]
+    limit: u32,
+    #[serde(default = "default_search")]
+    search: String,
+}
+
+fn default_page() -> u32 {
+    1
+}
+
+fn default_limit() -> u32 {
+    100
+}
+
+fn default_search() -> String {
+    String::new()
+}
+
+async fn get_cards(State(db): State<Arc<DbConnection>>, params: Query<CardQueryParams>) -> Json<Vec<Card>> {
+    let page = params.page;
+    let limit = params.limit;
+    let search = params.search.clone();
+
+    let conn = db.0.lock().await;
+
+    match search_cards(&conn, &search, page, limit) {
+        Ok(cards) => {
+            println!("Found cards: {:?}", cards);
+            Json(cards)
+        },
+        Err(e) => {
+            println!("Error finding cards: {:?}", e);
+            // TODO: logger the error
+            vec![].into()
+        }
+    }
 }
